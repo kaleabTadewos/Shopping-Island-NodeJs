6bbb4890e44120cd2802c9a0aa96120082d19175
kaleab tadewos ero <tadewoskaleab@gmail.com>
2020-07-01 22:13:10 -0500

    JEST - 3 : number of test should be greater than or equals to number of execution path

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,6 +1,16 @@
 const lib =  require('../testDemoFunctions/lib');
 
-test('it should return absolute value of a number' , () => {
+test('absolute - should return a positive number for positive input' , () => {
     const result = lib.absolute(1);
     expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number for negative input' , () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return a zero number for zero input' , () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
 });
