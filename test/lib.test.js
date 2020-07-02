7e4d1805e11ca9d107903b2f23ad8fa8d3fe8702
kaleab tadewos ero <tadewoskaleab@gmail.com>
2020-07-01 22:15:46 -0500

    JEST 4: better way of writing a test cases for an function using "describe and it" syntax

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,16 +1,18 @@
 const lib =  require('../testDemoFunctions/lib');
 
-test('absolute - should return a positive number for positive input' , () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-});
-
-test('absolute - should return a positive number for negative input' , () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
-
-test('absolute - should return a zero number for zero input' , () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
+describe('absolute' , () => {
+    it('absolute - should return a positive number for positive input' , () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('absolute - should return a positive number for negative input' , () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('absolute - should return a zero number for zero input' , () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
