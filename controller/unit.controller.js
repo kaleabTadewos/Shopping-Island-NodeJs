72cda7ab07571cbc4e1ef0418ba36dd7233eb541
kaleab tadewos ero <tadewoskaleab@gmail.com>
2020-07-03 03:26:07 -0500

    JEST 13 INTEGRATION TEST : Test unit findbyId

--- controller/unit.controller.js
+++ controller/unit.controller.js
@@ -16,13 +16,13 @@ exports.findById = async(req, res, next) => {
     const { error } = validateId({ _id: req.params.id });
     if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
     const unit = await Unit.findById(req.params.id);
-    if (!unit) return res.status(404).send(new ErrorResponse('400', 'no content found!'));
+    if (!unit) return res.status(404).send(new ErrorResponse('404', 'no content found!'));
     res.status(200).send(new ApiResponse(200, 'success', unit));
 };
 
 exports.findAll = async(req, res, next) => {
     const unit = await Unit.find();
-    if (!unit) return res.status(404).send(new ErrorResponse('400', 'no content found!'));
+    if (!unit) return res.status(404).send(new ErrorResponse('404', 'no content found!'));
     res.status(200).send(new ApiResponse(200, 'success', unit));
 }
 
@@ -44,6 +44,6 @@ exports.removeById = async(req, res, next) => {
     const { error } = validateId({ _id: req.params.id });
     if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
     const unit = await Unit.findByIdAndRemove(req.params.id);
-    if (!unit) return res.status(404).send(new ErrorResponse('400', 'no content found!'));
+    if (!unit) return res.status(404).send(new ErrorResponse('404', 'no content found!'));
     res.status(200).send(new ApiResponse(200, 'success', unit));
 };
