622f8b5f564aacc19bcb53876e218699d5bae5c8
kaleab tadewos ero <tadewoskaleab@gmail.com>
2020-07-03 01:41:36 -0500

    JEST 11: INTEGRATION TEST :     - for integration test , we need actual database access , it is not like unit test just to use mock functions. Here i just get connection string using config package not dotenv , so that i can put connectionString for test inside test.json file      - then when i test it automatically get connection string from test,json.      - but for other environment it get connection string from default.json.

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   environment: process.env.NODE_ENV,
-  port: process.env.PORT ,
-  connectionString:process.env.CONNECTION_STRING
+  port: process.env.PORT //,
+  //connectionString:process.env.CONNECTION_STRING
 };
